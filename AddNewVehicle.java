e4e40fad404a471dc9de0acc12e0496e6bb7a8b8
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-09-22 15:51:27 +0530

    modify

--- AddNewVehicle.java
+++ AddNewVehicle.java
@@ -11,7 +11,7 @@ class AddNewVehicle extends JFrame implements ActionListener {
 
     AddNewVehicle() {
         // Set up the frame
-        JFrame f = new JFrame("Add New Vehicle - Rent Roller Vehicle Rental Service");
+        setTitle("Add New Vehicle - Rent Roller Vehicle Rental Service");
 
         // Title Label
         JLabel title = new JLabel("Rent Roller Vehicle Rental Service");
@@ -77,32 +77,32 @@ class AddNewVehicle extends JFrame implements ActionListener {
         cancelButton.addActionListener(this);
 
         // Add components to the frame
-        f.add(title);
-        f.add(subtitle);
-        f.add(regNumberLabel);
-        f.add(regNumberField);
-        f.add(seatsLabel);
-        f.add(seatsField);
-        f.add(typeLabel);
-        f.add(typeField);
-        f.add(colorLabel);
-        f.add(colorField);
-        f.add(modelNumberLabel);
-        f.add(modelNumberField);
-        f.add(brandLabel);
-        f.add(brandField);
-        f.add(rentLabel);
-        f.add(rentField);
-        f.add(depositLabel);
-        f.add(depositField);
-        f.add(addVehicleButton);
-        f.add(cancelButton);
+        add(title);
+        add(subtitle);
+        add(regNumberLabel);
+        add(regNumberField);
+        add(seatsLabel);
+        add(seatsField);
+        add(typeLabel);
+        add(typeField);
+        add(colorLabel);
+        add(colorField);
+        add(modelNumberLabel);
+        add(modelNumberField);
+        add(brandLabel);
+        add(brandField);
+        add(rentLabel);
+        add(rentField);
+        add(depositLabel);
+        add(depositField);
+        add(addVehicleButton);
+        add(cancelButton);
 
         // Frame settings
-        f.setSize(650, 400);
-        f.setLayout(null);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(650, 400);
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     // Action listener for button events
